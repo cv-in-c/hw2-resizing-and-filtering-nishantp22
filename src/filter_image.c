bbaa88eb77e5230bad4da7135f3a249cec3cd498
Nishant Pandey <118831586+nishantp22@users.noreply.github.com>
2023-07-16 19:29:40 +0530

    Add files via upload

--- src/filter_image.c
+++ src/filter_image.c
@@ -6,90 +6,173 @@
 #include "image.h"
 #define TWOPI 6.2831853
 
-void l1_normalize(image im)
-{
-    // TODO
+void l1_normalize(image im){
+    float s=0;
+    for (int i=0;i<im.w*im.h*im.c;i++) s=s+im.data[i];
+    if (s==0) return;
+    else for (int i=0;i<im.w*im.c*im.h;i++){
+      im.data[i]=im.data[i]/s;
+    }
 }
 
-image make_box_filter(int w)
-{
-    // TODO
-    return make_image(1,1,1);
+image make_box_filter(int w){
+    image im=make_image(w, w, 1);
+    for (int i=0;i<w*w;i++){
+      im.data[i]=((float)1)/(w*w);
+    }
+    return im;
 }
 
-image convolve_image(image im, image filter, int preserve)
-{
-    // TODO
-    return make_image(1,1,1);
+
+image convolve_image(image im, image filter, int preserve){
+    assert(filter.c== 1||filter.c==im.c);
+    float a;
+    if(preserve==1) a=im.c;
+    else a=1;
+    image img=make_image(im.w,im.h,a);
+    int deltaH=filter.h/2;
+    int deltaW=filter.w/2;
+    for (int i=0;i<im.c;i++){
+      for (int j=-deltaH;j<im.h-deltaH;j++){
+        for (int k=-deltaW;k<im.w-deltaW;k++){
+          float f;
+          if(preserve==1){
+            f=0;
+          }
+          else f=get_pixel(img, k+deltaW, j+deltaH, 0);
+          for (int l=0;l<filter.h;l++){
+            for (int m=0;m<filter.w;m++){
+              float q;
+              if(filter.c==1){
+                q=0;
+              }
+              else q=i;
+              f=f+get_pixel(im,k+m,j+l,i)*\
+            get_pixel(filter,m,l,q);
+            }
+          }
+          float r;
+          if(preserve==1) r=i;
+          else r=0;
+          set_pixel(img,k+deltaW,j+deltaH,r,f);
+        }
+      }
+    }
+    return img;
 }
 
-image make_highpass_filter()
-{
-    // TODO
-    return make_image(1,1,1);
+image make_highpass_filter(){
+    image im = make_image(3, 3, 1);
+    float matrix[] = {0,-1,0,-1,4,-1,0,-1,0};
+    memcpy(im.data, matrix, sizeof(matrix));
+    return im;
 }
 
-image make_sharpen_filter()
-{
-    // TODO
-    return make_image(1,1,1);
+image make_sharpen_filter(){
+    image im = make_image(3, 3, 1);
+    float matrix[] = {0,-1,0,-1,5,-1,0,-1,0};
+    memcpy(im.data, matrix, sizeof(matrix));
+    return im;
 }
 
-image make_emboss_filter()
-{
-    // TODO
-    return make_image(1,1,1);
+image make_emboss_filter(){
+    image im=make_image(3, 3, 1);
+    float arr[] = {-2,-1,0,-1,1,1,0,1,2};
+    memcpy(im.data,arr,sizeof(arr));
+    return im;
 }
 
-// Question 2.2.1: Which of these filters should we use preserve when we run our convolution and which ones should we not? Why?
-// Answer: TODO
-
-// Question 2.2.2: Do we have to do any post-processing for the above filters? Which ones and why?
-// Answer: TODO
-
-image make_gaussian_filter(float sigma)
-{
-    // TODO
-    return make_image(1,1,1);
+image make_gaussian_filter(float sigma){
+    int w=6*sigma;
+    if(w>0) if(w%2!=1)w++;
+    else w=1;
+    image im=make_image(w,w,1);
+    int delta= w/2;
+    for (int i=-delta;i<w-delta;i++){
+      for (int j=-delta;j<w-delta;j++){
+        float f=1/(TWOPI*sigma*sigma)*exp((-i*i-j*j)/(2*sigma*sigma));
+        set_pixel(im,j+delta,i+delta,0,f);
+      }
+    }
+    return im;
 }
 
-image add_image(image a, image b)
-{
-    // TODO
-    return make_image(1,1,1);
+image add_image(image a, image b){
+    assert(a.h==b.h&&a.w==b.w&&a.c==b.c);
+    image im=make_image(a.w,a.h,a.c);
+    for (int i=0;i<a.c*a.h*a.w;i++){
+      im.data[i]=a.data[i]+b.data[i];
+    }
+    return im;
 }
 
-image sub_image(image a, image b)
-{
-    // TODO
-    return make_image(1,1,1);
+image sub_image(image a, image b){
+    assert(a.w==b.w&&a.h==b.h&&a.c==b.c);
+    image im=make_image(a.w,a.h,a.c);
+    for(int i=0;i<a.w*a.h*a.c;i++){
+      im.data[i]=a.data[i]-b.data[i];
+    }
+    return im;
 }
 
-image make_gx_filter()
-{
-    // TODO
-    return make_image(1,1,1);
+image make_gx_filter(){
+    image im=make_image(3, 3, 1);
+    float arr[] = {-1,0,1,-2,0,2,-1,0,1};
+    memcpy(im.data,arr, sizeof(arr));
+    return im;
 }
 
-image make_gy_filter()
-{
-    // TODO
-    return make_image(1,1,1);
+image make_gy_filter(){
+    image im=make_image(3, 3, 1);
+    float arr[]={-1,-2,-1,0,0,0,1,2,1};
+    memcpy(im.data,arr, sizeof(arr));
+    return im;
 }
 
-void feature_normalize(image im)
-{
-    // TODO
+void feature_normalize(image im){
+    float max=0;
+    float min=1;
+    for (int i=0;i<im.w*im.h*im.c;i++){
+      if(im.data[i]>max) max=im.data[i];
+      if(im.data[i]<min) min=im.data[i];
+    }
+    float delta=max-min;
+    if (delta!=0) for(int i=0;i<im.w*im.h*im.c;i++) im.data[i]=0; 
+    else for(int i=0;i<im.w*im.h*im.c;i++) im.data[i]=(im.data[i]-min)/delta;
 }
 
-image *sobel_image(image im)
-{
-    // TODO
-    return calloc(2, sizeof(image));
+image *sobel_image(image im){
+    int i = 0;
+    image x=make_gx_filter();
+    image y=make_gy_filter();
+    image *img=malloc(2*sizeof(image));
+    image convx=convolve_image(im,x,0);
+    image convy=convolve_image(im,y,0);
+    image en=make_image(convx.w,convx.h,1);
+    image new=en;
+    for (int i=0;i<im.w*im.h;i++){
+      en.data[i] = sqrt(convx.data[i]*convx.data[i] + convy.data[i]*convy.data[i]);
+      new.data[i] = atan2(convy.data[i], x.data[i]);
+    }
+    free_image(x);
+    free_image(y);
+    free_image(convx);
+    free_image(convy);
+    img[0]=en;
+    img[1]=new;
+    return img;
 }
 
-image colorize_sobel(image im)
-{
-    // TODO
-    return make_image(1,1,1);
+image colorize_sobel(image im){
+    image new=make_image(im.w, im.h,3);
+    image *img=sobel_image(im);
+    feature_normalize(img[0]);
+    feature_normalize(img[1]);
+    memcpy(new.data,img[1].data,im.w*im.h*sizeof(float));
+    memcpy(new.data+im.w*im.h,img[0].data,im.w*im.h*sizeof(float));
+    memcpy(new.data+2*im.h*im.w,img[0].data,im.w*im.h*sizeof(float));
+    free_image(img[0]);
+    free_image(img[1]);
+    hsv_to_rgb(new);
+    return new;
 }
--- src/resize_image.c
+++ src/resize_image.c
@@ -4,24 +4,58 @@
 float nn_interpolate(image im, float x, float y, int c)
 {
     // TODO Fill in
-    return 0;
+    return get_pixel(im, round(x), round(y), c);    
 }
 
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image im=make_image(w, h, im.c);
+    float hRes=(float)im.h/h;
+    float wRes=(float)im.w/w;
+    for (int i=0;i<im.c;i++){
+      for (int j=0; j<h;j++){
+        for (int k=0; k<w;k++){
+          float hNew=hRes*(j+(1/2))-(1/2);
+          float wNew=wRes*(k+(1/2))-(1/2);
+          float f=nn_interpolate(im,wNew,hNew,i);
+          set_pixel(im,k,j,i,f);
+        }
+      }
+    }
+    return im;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
     // TODO
-    return 0;
+    float p=get_pixel(im,floor(x),floor(y),c);
+    float q=get_pixel(im,ceil(x),floor(y),c);
+    float r=get_pixel(im,floor(x),ceil(y),c);
+    float s=get_pixel(im,ceil(x),ceil(y),c);
+    float t=q*(x-floor(x))+p*(ceil(x)-x);
+    if(floor(x)==ceil(x)) t=p;
+    float u=s*(x-floor(x))+r*(ceil(x)-x);
+    if(floor(x)==ceil(x)) u=r;
+    float q=u*(y-floor(y))+t*(ceil(y)-y);
+    if(floor(y)==ceil(y)) q=t;
+    return q;
 }
-
 image bilinear_resize(image im, int w, int h)
 {
     // TODO
-    return make_image(1,1,1);
+    image img=make_image(w, h, im.c);
+    float hBil=(float)im.h/h;
+    float wBil=(float)im.w/w;
+    for (int i=0;i<im.c;i++){
+      for (int j=0;j<h;j++){
+        for (int k=0; k<w;k++){
+          float hNew=hBil*(j+(1/2))-(1/2);
+          float wNew=wBil*(k+(1/2))-(1/2);
+          float v=bilinear_interpolate(im,wNew,hNew,i);
+          set_pixel(img,k,j,i,v);
+        }
+      }
+    }
+    return img;
 }
-
